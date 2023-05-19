4f03f269a6eed3b3bcebbc316aecbcbece43a74f
Holly <hcreek@protonmail.com>
2023-05-18 22:55:53 -0400

    remove unnecessary request, edit json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Container from 'react-bootstrap/Container';
+import Stack from 'react-bootstrap/Stack';
 import Header from "./components/Header";
 import SelectCptCode from "./components/SelectCptCode";
 import CostList from "./components/CostList";
 import AddCost from "./components/AddCost";
-import Stack from 'react-bootstrap/Stack';
 
 function App() {
-  const [cptCodes, setCptCodes] = useState([]);
   const [selectedCptCode, setSelectedCptCode] = useState(null);
   const [costs, setCosts] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/cptCodes").then((response) => {
-      setCptCodes(response.data);
-    });
-  }, []);
 
   const onSelectCptCode = (codeId) => {
     setSelectedCptCode(codeId);
@@ -34,7 +27,7 @@ function App() {
       <Header />
       <Container className="app py-4">
         <Stack gap={5}>
-          <SelectCptCode cptCodes={cptCodes} onSelectCptCode={onSelectCptCode} />
+          <SelectCptCode onSelectCptCode={onSelectCptCode} />
           {selectedCptCode && (
             <> 
               <CostList costs={costs} />
